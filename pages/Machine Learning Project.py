e3bf5b912499a7b1069372019626cb85defba3e3
Taahaa-Dawe <145599886+Taahaa-Dawe@users.noreply.github.com>
2024-02-02 11:39:12 -0700

    Update Machine Learning Project.py

--- pages/Machine Learning Project.py
+++ pages/Machine Learning Project.py
@@ -94,7 +94,7 @@ def DataCleaning():
 	st. write("""
  	<ul> Kaggle AB Testing Dataset:
 		<li> A null value in every column of the Kaggle dataset (marked as NaN) was identified and removed, as it didn't provide valuable information.</li>
-		<li> The decision to exclude this column aligns with the principle of maintaining data integrity by eliminating irrelevant or non-contributory features. </li>
+		<li> The decision to exclude this row aligns with the principle of maintaining data integrity by eliminating irrelevant or non-contributory features. </li>
   	</ul>
 
 """ 	, unsafe_allow_html=True)
@@ -103,7 +103,11 @@ def DataCleaning():
 
 """,
 	 unsafe_allow_html=True)
-
+	st.subheader("Visual Exploration:")
+	st.write("""
+ 	<div class='tableauPlaceholder' id='viz1706825440189' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;AB&#47;ABtestAnalysis&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='ABtestAnalysis&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;AB&#47;ABtestAnalysis&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1706825440189');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='2227px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='2227px';} else { vizElement.style.width='100%';vizElement.style.height='3127px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+ """
+ 	, unsafe_allow_html=True)
 
 
 
