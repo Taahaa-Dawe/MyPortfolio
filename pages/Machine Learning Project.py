af2b724c1ac29a61865db1f02237680319841b1b
Taahaa-Dawe <145599886+Taahaa-Dawe@users.noreply.github.com>
2024-03-28 16:21:13 -0600

    Update Machine Learning Project.py

--- pages/Machine Learning Project.py
+++ pages/Machine Learning Project.py
@@ -129,6 +129,8 @@ The dataset was divided into two subsets: a training dataset comprising 70% of t
 <a href="https://ibb.co/bBhNKgY"><img src="https://i.ibb.co/KqZVrjB/image.png" alt="image" border="0"></a>
 </center>
 <center>Test Data</center>
+
+<li> <a href ="https://github.com/Taahaa-Dawe/OnlineDatingReview_TextMiningProject/blob/main/Wholedata.csv"> Link to Data </a>
 	""",unsafe_allow_html=True)
 
 
