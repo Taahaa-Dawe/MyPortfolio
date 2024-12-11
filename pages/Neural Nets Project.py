d6b3ee9c1b6fefa9f818920e20ab120779610a5d
Taahaa-Dawe <145599886+Taahaa-Dawe@users.noreply.github.com>
2024-12-11 15:17:13 -0700

    Update Neural Nets Project.py

--- pages/Neural Nets Project.py
+++ pages/Neural Nets Project.py
@@ -29,10 +29,10 @@ def Intro():
 	
 	st.write("""<center><a href="https://ibb.co/LvLK613"><img src="https://i.ibb.co/d7dZ6DR/image.png" alt="image" border="0"></a><br />
  Figure 3: Linguistic Map of Morocco – 3 main languages
- (<a href="https://www.reddit.com/r/MapPorn/comments/p4ywvf/distribution_of_berberamazigh_languages_in_morocco/"> left</a>) and 3 Amazigh dialects ( <a href ="https://www.instagram.com/amazighworldnews/p/C8FoLiwuJ59/">right</a>).<br /></center>""", unsafe_allow_html=True)
+ (<a href="https://www.reddit.com/r/MapPorn/comments/p4ywvf/distribution_of_berberamazigh_languages_in_morocco/">left</a>) and 3 Amazigh dialects (<a href ="https://www.instagram.com/amazighworldnews/p/C8FoLiwuJ59/">right</a>).<br /></center>""", unsafe_allow_html=True)
 	st.write("")
 	st.write("""Approximately 7.5 million Amazigh speakers reside in Morocco (<a href ="https://www.crossroadsculturalexchange.com/blog/about-amazigh">Crossroads Cultural Exchange</a>). According to <a href ="https://www.euronews.com/2023/01/31/morocco-amazigh">EuroNews </a>, Amazigh is spoken by 25% – 30% of the population, but its use is declining among younger generations. Although Amazigh is taught in some schools, the percentage of children learning the language has dropped from 14% in 2010 to just 9% in 2023. This decline can be attributed to the limited teaching in schools and the increasing use of Romanized Tifinagh, which overshadows its original script. """, unsafe_allow_html=True)
-	st.write("""<center><a href="https://ibb.co/6PqD6B5"><img src="https://i.ibb.co/jMj37RF/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 4: Students in a Moroccan classroom with names in Tifinagh <a href = "https://www.instagram.com/amazighworldnews/p/C7rcG3cMg7O/?img_index=1">(Amazighworldnews).</a></a><br /> </center>""", unsafe_allow_html=True)
+	st.write("""<center><a href="https://ibb.co/6PqD6B5"><img src="https://i.ibb.co/jMj37RF/image.png" alt="image" border="0"></a><br />Figure 4: Students in a Moroccan classroom with names in Tifinagh <a href = "https://www.instagram.com/amazighworldnews/p/C7rcG3cMg7O/?img_index=1">(Amazighworldnews).</a><br /> </center>""", unsafe_allow_html=True)
 	st.write("")
 	st.write("""As a consequence, many Moroccans with Amazigh origins are unable to speak the language. Worsening the fact that the language is not largely taught in schools, most people write it in its original letters Tifinagh but rather use the romanized version, which might actually be another contributor to why it has not yet died.""", unsafe_allow_html=True)
 	st.write("""The Expanded Graded Intergenerational Disruption Scale (EGIDS) is a tool used to assess the status of a language, measuring its level of endangerment or development (Ethnologue). The scale helps highlight a language’s strongest and safest areas of use while offering insights into its long-term viability. Standard Moroccan Tamazight is classified at EGIDS Level 1 (National), meaning it is used in education, work, media, and government in Morocco. In the language cloud, a large colored dot represents Tamazight’s institutional support, corresponding to EGIDS Levels 0 – 4 <a href ="https://www.ethnologue.com/faq/what-is-egids/">(Ethnologue)</a>.""", unsafe_allow_html=True)
@@ -49,28 +49,28 @@ def DataPreparation():
 	st.write("""The data consists of two files. File 1, sourced from <a href ="https://fr.wikipedia.org/wiki/Tifinagh">Wikipedia</a>, contains 31 letters of the Tifinagh alphabet. File 2, sourced from <a href ="https://tatoeba.org/en/downloads">Tatoeba</a>, comprises Amazigh-English sentence pairs, originally totaling 15,371 rows before cleaning and the same number after cleaning. """,unsafe_allow_html=True)
 	st.write("")
 	st.write("""<center> 
- <a href="https://ibb.co/StTZDw9"><img src="https://i.ibb.co/fkRTVdP/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 6: Tifinagh from Wikipedia with 31 letters in total.</a><br />
+ <a href="https://ibb.co/StTZDw9"><img src="https://i.ibb.co/fkRTVdP/image.png" alt="image" border="0"></a><br />Figure 6: Tifinagh from Wikipedia with 31 letters in total.</a><br />
  	</center>""",unsafe_allow_html=True)
 	st.write("")
 	st.write("""<center> 
- 	<a href="https://ibb.co/10tVG6D"><img src="https://i.ibb.co/TwxXMLV/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 7: Sentence Pairs (Amazigh – English) from Tatoeba with 15,371 rows (before cleaning).</a><br />
+ 	<a href="https://ibb.co/10tVG6D"><img src="https://i.ibb.co/TwxXMLV/image.png" alt="image" border="0"></a><br />Figure 7: Sentence Pairs (Amazigh – English) from Tatoeba with 15,371 rows (before cleaning).</a><br />
   	</center>""",unsafe_allow_html=True)
 	st.write("")
 	st.write(""" No exact duplicates were found in File 2. However, duplicates in English translations (due to dialectal variations) and Amazigh translations were found and removed, resulting in the deletion of 4,701 rows, accounting for 30.58% of the data. The remaining sentences were standardized to include both romanized and Tifinagh scripts, with the romanization process performed using File 1. The Tamazight vocabulary in the cleaned dataset contains 7,161 unique words, compared to 3,929 unique English words.""",unsafe_allow_html=True)
 	st.write("")
-	st.write("""<center> <a href="https://ibb.co/Wcjc6Dy"><img src="https://i.ibb.co/wKjKRJW/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 8: Sentence Pairs (Amazigh – English) from Tatoeba with 10,670 rows (after cleaning).</a><br /> </center>""",unsafe_allow_html=True)
+	st.write("""<center> <a href="https://ibb.co/Wcjc6Dy"><img src="https://i.ibb.co/wKjKRJW/image.png" alt="image" border="0"></a><br />Figure 8: Sentence Pairs (Amazigh – English) from Tatoeba with 10,670 rows (after cleaning).</a><br /> </center>""",unsafe_allow_html=True)
 	st.write("")
 	st.subheader("Data Exploration")
-	st.write("""<center><a href="https://ibb.co/YdY45JM"><img src="https://i.ibb.co/2Mm0zRQ/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 9: English (Left) and Tamazight’s (Right) count of words in descending order.</a><br /></center>""",unsafe_allow_html=True)
+	st.write("""<center><a href="https://ibb.co/YdY45JM"><img src="https://i.ibb.co/2Mm0zRQ/image.png" alt="image" border="0"></a><br />Figure 9: English (Left) and Tamazight’s (Right) count of words in descending order.</a><br /></center>""",unsafe_allow_html=True)
 	st.write("")
 	st.subheader("English Sentences in the Dataset")
 	st.write("""An analysis of the English sentences in the dataset reveals potential challenges for training. A high frequency of stopwords, pronouns, and people’s names such as “Tom” dominates the text. This imbalance might hinder the model’s ability to learn meaningful patterns, as these elements contribute little to understanding the structural and semantic nuances of the language. """,unsafe_allow_html=True)
-	st.write("""<center><a href="https://imgbb.com/"><img src="https://i.ibb.co/Jd3GKKG/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 10: WordCloud of English Sentences in the Dataset.</a><br /></center>""",unsafe_allow_html=True)
+	st.write("""<center><a href="https://imgbb.com/"><img src="https://i.ibb.co/Jd3GKKG/image.png" alt="image" border="0"></a><br />Figure 10: WordCloud of English Sentences in the Dataset.</a><br /></center>""",unsafe_allow_html=True)
 	st.write("")
 	st.subheader("Tamazight Sentences in the Dataset")
 	st.write("""A similar trend is observed in the Tamazight sentences, as demonstrated by a word cloud of the romanized Amazigh text. Stopwords and pronouns are prevalent, with “Tom” (romanized as “tum”) appearing frequently, although less so than in the English dataset. This overrepresentation poses a challenge for training, as it may skew the model’s focus away from more informative linguistic features.""",unsafe_allow_html=True)
 	st.write("")
-	st.write("""<center><a href="https://imgbb.com/"><img src="https://i.ibb.co/8D0mfkW/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 11: WordCloud of English Sentences in the Dataset</a><br /></center>""",unsafe_allow_html=True)
+	st.write("""<center><a href="https://imgbb.com/"><img src="https://i.ibb.co/8D0mfkW/image.png" alt="image" border="0"></a><br />Figure 11: WordCloud of English Sentences in the Dataset</a><br /></center>""",unsafe_allow_html=True)
 	st.write("")
 	st.subheader("Data Prep and EDA:")
 	st.write("""https://drive.google.com/drive/folders/1QvSNVwY0pc176zVzGE9nJ_hYTw3n23py?usp=share_link """,unsafe_allow_html=True)
@@ -80,25 +80,25 @@ def Methodology():
 	st.subheader("Data Preparation")
 	st.write("""The first step in the process was to collect and prepare the data for training the models. A parallel dataset containing sentence pairs in Amazigh (as input) and English (as output) was sourced to provide the foundation for the translation task. This dataset served as the basis for both training and evaluating the models’ performance.""")
 	st.write("""<center>
- <a href="https://ibb.co/VBVzxZd"><img src="https://i.ibb.co/RH2sNm1/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 12: Sample from cleaned dataset with Tamazight in Tifinagh (left) and English (right).</a><br />
+ <a href="https://ibb.co/VBVzxZd"><img src="https://i.ibb.co/RH2sNm1/image.png" alt="image" border="0"></a><br />Figure 12: Sample from cleaned dataset with Tamazight in Tifinagh (left) and English (right).</a><br />
  	</center>""",unsafe_allow_html=True)
 	st.write("")
 	st.write("""Data cleaning was a critical part of the preparation process. Duplicate and irrelevant entries were removed to improve the quality and relevance of the dataset. The Amazigh text was standardized by replacing Amazigh characters with their Romanized equivalents, as the models lacked support for the Tifinagh script. Additionally, punctuation and formatting inconsistencies were cleaned to ensure uniformity across the dataset, reducing potential noise during training. """,unsafe_allow_html=True)
-	st.write("""<center><a href="https://imgbb.com/"><img src="https://i.ibb.co/ys4wXHJ/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 13: Sample from cleaned dataset with Romanized Tamazight.</a><br /></center>""",unsafe_allow_html=True)
+	st.write("""<center><a href="https://imgbb.com/"><img src="https://i.ibb.co/ys4wXHJ/image.png" alt="image" border="0"></a><br />Figure 13: Sample from cleaned dataset with Romanized Tamazight.</a><br /></center>""",unsafe_allow_html=True)
 	st.write("")
 	st.write("""Tokenization was then applied to break the sentences into smaller units, or tokens, which enabled the models to process them effectively. Each token was converted into a numerical representation suitable for input to the machine learning models. To maintain uniform input lengths, sentences were padded, meeting the structural requirements for training. This data preparation process ensured the dataset was optimized for use in building accurate and reliable translation models.""",unsafe_allow_html=True)
 	st.write("")
-	st.write("""<center><a href="https://imgbb.com/"><img src="https://i.ibb.co/Bzx9DNy/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 14: Sample of tokenized and padded data.</a><br /></center>""",unsafe_allow_html=True)
+	st.write("""<center><a href="https://imgbb.com/"><img src="https://i.ibb.co/Bzx9DNy/image.png" alt="image" border="0"></a><br />Figure 14: Sample of tokenized and padded data.</a><br /></center>""",unsafe_allow_html=True)
 	st.write("")
 	st.subheader("Models and Techniques")
 	st.write("""To translate Amazigh to English, several models and techniques were employed. The Sequence-to-Sequence (Seq2Seq) model served as the baseline, leveraging an encoder-decoder architecture. The encoder processed Amazigh input sentences and converted them into a “context vector,” which the decoder then used to predict English translations one word at a time. While this model was effective for simpler sentences, it struggled with longer and more complex structures due to its limited ability to capture long-range dependencies.""",unsafe_allow_html=True)
 	st.write("")
 	st.write("""<center> 
- <a href="https://ibb.co/bPWc0wv"><img src="https://i.ibb.co/nbgxGd1/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 15: Architecture of a Sequence-to-Sequence Model (Github).</a><br />
+ <a href="https://ibb.co/bPWc0wv"><img src="https://i.ibb.co/nbgxGd1/image.png" alt="image" border="0"></a><br />Figure 15: Architecture of a Sequence-to-Sequence Model (Github).</a><br />
  </center>""",unsafe_allow_html=True)
 	st.write("")
 	st.write(""" Transformer-based models were also implemented to handle the translation task more effectively. A custom Transformer model was built from scratch, utilizing the attention mechanism to focus on relevant parts of input sentences during translation. The model incorporated key components such as multi-head attention, which helped identify relationships between words, positional encoding to ensure the model recognized the order of words, and feed-forward neural networks to enhance encoded representations for accurate translations. This custom model improved translation quality but required extensive training on the dataset""",unsafe_allow_html=True)
-	st.write(""" <center><a href="https://ibb.co/2NRHRKm"><img src="https://i.ibb.co/k1zvzck/image.png" alt="image" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>Figure 16: Architecture of a Transformer Model (Attention is All you Need).</a><br /></center>""",unsafe_allow_html=True)
+	st.write(""" <center><a href="https://ibb.co/2NRHRKm"><img src="https://i.ibb.co/k1zvzck/image.png" alt="image" border="0"></a><br />Figure 16: Architecture of a Transformer Model (Attention is All you Need).</a><br /></center>""",unsafe_allow_html=True)
 	st.write("")
 	st.write("""Additionally, a pre-trained Transformer model, Helsinki-NLP/opus-mt-en-ro from Hugging Face, was fine-tuned for the Amazigh-English translation task. Pre-trained on multilingual datasets, this model was faster and more efficient to adapt, as it already had a robust understanding of English. Fine-tuning allowed it to specialize in translating Amazigh sentences while maintaining high translation accuracy and fluency, outperforming the custom Transformer in both efficiency and output quality.""",unsafe_allow_html=True)
 	st.write("""The Google Translate API was used as a benchmark for comparison with the custom and pre-trained models. Translation requests were sent via API calls to the service, which performed well for general translations. However, the API struggled with Amazigh-specific linguistic and cultural nuances, highlighting the limitations of generalized translation tools for low-resource languages.""",unsafe_allow_html=True)
