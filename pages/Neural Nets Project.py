5f2f800da269da65d17fc2ae9419993f03b64e24
Taahaa-Dawe <145599886+Taahaa-Dawe@users.noreply.github.com>
2024-12-11 12:09:10 -0700

    Update Neural Nets Project.py

--- pages/Neural Nets Project.py
+++ pages/Neural Nets Project.py
@@ -11,37 +11,40 @@ def select(option):
 def Abstract():
 	st.header("Abstract", divider='blue')
 	st.write(""" Amazigh languages, spoken by 14 million people across North Africa, face challenges in preservation and technological integration. This project develops an Amazigh-to-English translation system using Seq2Seq, Transformer, fine-tuned Helsinki-NLP models, and Google Translate API. The fine-tuned pre-trained Transformer Helsinki-NLP model achieved the highest BLEU score (49.27), highlighting its potential for under-resourced languages, despite having only Romanized support. Challenges faced in this project include limited data and lack of Tifinagh script support. This emphasizes the need for more resources to preserve Amazigh’s linguistic identity through technology.""")
+
+def Intro():
+
+def DataPreparation():
+
+
+def Methodology():
+
+def AnalysisResults():
+
+def Conclusions():
+	
 def DisplayPart(option):
 	if option == "Abstract":
 		Abstract()
 	elif option == "Introduction":
 		Intro()
-	elif option == "DataPrep/EDA":
-		DataCleaning()
-	elif option == "Clustering":
-		Clustering()
-	elif option == "ARM":
-		ARM()
-	elif option =="LDA":
-		LDA()
-	elif option == "SVM":
-		SVM()
-	elif option == "DT":
-		DT()
-	elif option == "NB":
-		NB()
-	elif option =="NN":
-		NN()
+	elif option == "Data Preparation":
+		DataPreparation()
+	elif option == "Methodology":
+		Methodology()
+	elif option == "Analysis and Results":
+		AnalysisResults()
 	elif option =="Conclusions":
 		Conclusions()
 	else:
 		st.write("Work in Progress")
+
 st.title("Amazigh to English Language Translation | Preserving and Honoring the Amazigh Identity.")
 
 
 header = st.container()
 with header:
-	option = st.selectbox("", ("Abstract","Introduction", "DataPrep/EDA", "Clustering", "ARM","LDA","DT","NB","SVM", "NN","Conclusions"))
+	option = st.selectbox("", ("Abstract","Introduction", "Data Preparation", "Methodology", "Analysis and Results","Conclusions"))
 header.write("""<div class='fixed-header'/>""", unsafe_allow_html=True)
 select(option)
 ### Custom CSS for the sticky header
