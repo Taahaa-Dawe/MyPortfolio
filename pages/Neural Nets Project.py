5a1eefaf9920523f53d348075c3647470eda02fb
Taahaa-Dawe <145599886+Taahaa-Dawe@users.noreply.github.com>
2024-12-11 11:41:14 -0700

    Update Neural Nets Project.py

--- pages/Neural Nets Project.py
+++ pages/Neural Nets Project.py
@@ -1,9 +1,1 @@
-import streamlit as st
-import pandas as pd
-from PIL import Image
-from PIL import Image
-import streamlit.components.v1 as components
-option = ""
-
-def select(option):
-	DisplayPart(option)
+print("Hello World")
